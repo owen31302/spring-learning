22ea41172fae1704ca5358650136a9b729e11e6f
yuchlin <yuchlin@ebay.com>
2019-03-24 12:52:20 -0700

    Hibernate Advanced Mappings - Eager vs Lazy Loading.

---
+++ src/main/java/com/owen/springtest/hibernatemapping/eager_vs_lazy/demo/FetchJoinDemo.java
@@ -0,0 +1,75 @@
+package com.owen.springtest.hibernatemapping.eager_vs_lazy.demo;
+
+
+import com.owen.springtest.hibernatemapping.one_to_many.entity.Course;
+import com.owen.springtest.hibernatemapping.one_to_many.entity.Instructor;
+import com.owen.springtest.hibernatemapping.one_to_many.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+
+public class FetchJoinDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+
+			// start a transaction
+			session.beginTransaction();
+
+			// Resolve the issue: Option2 -> Hibernate query with HQL
+
+			// get the instructor from db
+			int theId = 1;
+
+			Query<Instructor> query =
+							session.createQuery("select i from Instructor i " +
+									      			"join fetch i.courses " +
+													"where i.id=:theInstructorId",
+							Instructor.class);
+
+			// set parameter on query
+			query.setParameter("theInstructorId", theId);
+
+			Instructor instructor = query.getSingleResult();
+
+			System.out.println("[Owen] Instructor: " + instructor);
+
+
+			// commit transaction
+			session.getTransaction().commit();
+
+			session.close();
+
+			System.out.println("[Owen] Courses: " + instructor.getCourses());
+
+			System.out.println("[Owen] Done!");
+
+
+		}
+		finally {
+			session.close();
+
+			factory.close();
+		}
+	}
+
+}
+
+
+
+
+
--- src/main/java/com/owen/springtest/hibernatemapping/one_to_many/entity/Instructor.java
+++ src/main/java/com/owen/springtest/hibernatemapping/one_to_many/entity/Instructor.java
@@ -40,7 +40,9 @@ public class Instructor {
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail instructorDetail;
 
-	@OneToMany(mappedBy = "instructor", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+	@OneToMany(fetch = FetchType.LAZY,
+			   mappedBy = "instructor",
+			   cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
 	private List<Course> courses;
 	
 	public Instructor() {
